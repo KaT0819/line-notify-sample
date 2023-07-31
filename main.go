756b8418a9e8d38c88dae238ee737acf31edb9be
katsuyuki.toki <katuyuki.toki@gmail.com>
2023-07-31 19:33:21 +0900

    ImageFile指定ありとなしの場合でリクエストパラメータがやヘッダーが異なるため、分岐

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -32,47 +33,67 @@ func main() {
 	formData.Set("stickerId", "16581242")
 	// formData.Set("notificationDisabled", "true") // 通知を送らない場合true
 
-	// ファイルを開く
-	file, err := os.Open("sample_image.jpeg")
-	if err != nil {
-		log.Fatalf("Error opening file: %s", err)
-	}
-	defer file.Close()
+	// 画像ファイルを送信する場合はtrue
+	withImageFile := false
+	var req *http.Request
 
-	// リクエストボディをバッファに読み込む
-	var requestBody bytes.Buffer
-	multipartWriter := multipart.NewWriter(&requestBody)
-
-	// ファイルをフォームデータに追加
-	fileField := "imageFile"
-	filePart, err := multipartWriter.CreateFormFile(fileField, "sample_image.jpeg")
-	if err != nil {
-		log.Fatalf("Error creating form file: %s", err)
-	}
-	_, err = io.Copy(filePart, file)
-	if err != nil {
-		log.Fatalf("Error copying file content: %s", err)
-	}
-
-	// 他のフォームデータを追加
-	for key, values := range formData {
-		for _, value := range values {
-			_ = multipartWriter.WriteField(key, value)
+	if withImageFile {
+		// ファイルを開く
+		file, err := os.Open("sample_image.jpeg")
+		if err != nil {
+			log.Fatalf("Error opening file: %s", err)
 		}
+		defer file.Close()
+
+		// リクエストボディをバッファに読み込む
+		var requestBody bytes.Buffer
+		multipartWriter := multipart.NewWriter(&requestBody)
+
+		// ファイルをフォームデータに追加
+		fileField := "imageFile"
+		filePart, err := multipartWriter.CreateFormFile(fileField, "sample_image.jpeg")
+		if err != nil {
+			log.Fatalf("Error creating form file: %s", err)
+		}
+		_, err = io.Copy(filePart, file)
+		if err != nil {
+			log.Fatalf("Error copying file content: %s", err)
+		}
+
+		// 他のフォームデータを追加
+		for key, values := range formData {
+			for _, value := range values {
+				_ = multipartWriter.WriteField(key, value)
+			}
+		}
+
+		// リクエストボディをクローズ
+		multipartWriter.Close()
+
+		// POSTリクエストを作成
+		req, err = http.NewRequest("POST", apiURL, &requestBody)
+		if err != nil {
+			log.Fatalf("Error creating request: %s", err)
+		}
+
+		// ヘッダーを設定
+		req.Header.Set("Authorization", "Bearer "+accessToken)
+		req.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+
+	} else {
+		formData.Set("imageThumbnail", "https://img.freepik.com/free-photo/yasaka-pagoda-sannen-zaka-street-kyoto-japan_335224-12.jpg")
+		formData.Set("imageFullsize", "https://img.freepik.com/free-photo/yasaka-pagoda-sannen-zaka-street-kyoto-japan_335224-12.jpg")
+
+		// POSTリクエストを作成
+		req, err = http.NewRequest("POST", apiURL, strings.NewReader(formData.Encode()))
+		if err != nil {
+			log.Fatalf("Error creating request: %s", err)
+		}
+
+		// ヘッダーを設定
+		req.Header.Set("Authorization", "Bearer "+accessToken)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	}
-
-	// リクエストボディをクローズ
-	multipartWriter.Close()
-
-	// POSTリクエストを作成
-	req, err := http.NewRequest("POST", apiURL, &requestBody)
-	if err != nil {
-		log.Fatalf("Error creating request: %s", err)
-	}
-
-	// ヘッダーを設定
-	req.Header.Set("Authorization", "Bearer "+accessToken)
-	req.Header.Set("Content-Type", multipartWriter.FormDataContentType())
 
 	// HTTPクライアントを作成
 	client := &http.Client{}
